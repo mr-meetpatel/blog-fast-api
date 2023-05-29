023b14fce1cf24f0fb57b0f1f24ec50919eb9e18
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-05-29 14:09:37 +0530

    feat: Add response model for API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, status, HTTPException, Depends
 from pydantic import BaseModel
+from typing import List
 from psycopg2.extras import RealDictCursor
-from . import models
+from . import models, schemas
 from .database import engine, get_db, session
 import psycopg2
 import time
@@ -9,12 +10,6 @@ import time
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    is_published: bool
 
 
 while True:
@@ -39,21 +34,16 @@ async def home():
     return {"message": "Welcome..."}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.PostResponse])
 async def get_posts(db: session = Depends(get_db)):
     # cursor.execute(""" SELECT * FROM posts; """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"details": posts}
+    return posts
 
 
-@app.get("/db")
-def test_db_connection(db: session = Depends(get_db)):
-    return {"status": "success"}
-
-
-@app.get("/posts/{id}")
-async def get_posts(id: int,db: session = Depends(get_db)):
+@app.get("/posts/{id}", response_model=schemas.PostResponse)
+async def get_posts(id: int, db: session = Depends(get_db)):
     # cursor.execute(
     #     """ SELECT * FROM posts WHERE id=%s; """,
     #     (str(id),),
@@ -67,11 +57,13 @@ async def get_posts(id: int,db: session = Depends(get_db)):
             detail=f"No Result Found For Given id : {id}",
         )
 
-    return {"details": post}
+    return post
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_post(payload: Post, db: session = Depends(get_db)):
+@app.post(
+    "/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse
+)
+async def create_post(payload: schemas.CreatePost, db: session = Depends(get_db)):
     # cursor.execute(
     #     """INSERT INTO posts("title","content","is_published") VALUES (%s,%s,%s) RETURNING *""",
     #     (payload.title, payload.content, payload.is_published),
@@ -86,7 +78,7 @@ async def create_post(payload: Post, db: session = Depends(get_db)):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int,db: session = Depends(get_db)):
+async def delete_post(id: int, db: session = Depends(get_db)):
     # cursor.execute(
     #     """ DELETE FROM posts WHERE id=%s RETURNING *; """,
     #     (str(id)),
@@ -102,8 +94,10 @@ async def delete_post(id: int,db: session = Depends(get_db)):
     db.commit()
 
 
-@app.put("/posts/{id}")
-async def update_post(id: int, payload: Post,db: session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.PostResponse)
+async def update_post(
+    id: int, payload: schemas.CreatePost, db: session = Depends(get_db)
+):
     # cursor.execute(
     #     "UPDATE posts SET title=%s,content=%s,is_published=%s WHERE id = %s RETURNING *",
     #     (
@@ -122,6 +116,6 @@ async def update_post(id: int, payload: Post,db: session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"No Result Found For Given id : {id}",
         )
-    update_query.update(payload.dict(),synchronize_session=False)
+    update_query.update(payload.dict(), synchronize_session=False)
     db.commit()
-    return {"details": update_query.first()}
+    return update_query.first()
