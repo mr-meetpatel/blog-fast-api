aa6b3e2b1321e69ca79a6dfe93f68eee280142b0
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-05-29 18:23:18 +0530

    feat: Add response model for users API

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 
 class Post(BaseModel):
     title: str
     content: str
-    is_published: bool = True
 
 
 class CreatePost(Post):
@@ -14,6 +13,22 @@ class CreatePost(Post):
 
 class PostResponse(Post):
     id: int
+    is_published: bool = True
+    created_at: datetime
+
+
+    class Config:
+        orm_mode = True
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool =True
     created_at: datetime
 
     class Config:
