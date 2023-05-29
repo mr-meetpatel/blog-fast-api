7ade2a930b619fe9a7bea39bdafcc72ec9f889d2
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-05-29 18:24:42 +0530

    feat: Create User database model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,26 @@
 from .database import Base
-from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+
+
 class Post(Base):
     __tablename__ = "posts"
-    id = Column(Integer,primary_key=True,nullable=False)
-    title = Column(String,nullable=False)
-    content = Column(String,nullable=False)
-    is_published = Column(Boolean,server_default="False")
-    created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('NOW()'))
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    is_published = Column(Boolean, server_default="False")
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()")
+    )
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, server_default="True")
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()")
+    )
