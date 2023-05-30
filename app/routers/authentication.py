17d2c76548c9f104ff4ce21038e2fb38f63b0f9d
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-05-30 12:21:44 +0530

    refactor: Update authentication API response to include JWT access token

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -1,17 +1,17 @@
 from fastapi import status, HTTPException, Depends, APIRouter
-from typing import List
-from app import models, schemas, utils
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from app import models, schemas, utils,oauth2
 from app.database import get_db, session
 
 router = APIRouter(prefix="/api/v1", tags=["Authentication"])
 
 
 @router.post("/login")
-def login(payload: schemas.UserLogin, db: session = Depends(get_db)):
+def login(payload: OAuth2PasswordRequestForm = Depends(), db: session = Depends(get_db)):
     user = (
         db.query(models.User)
         .filter(
-            models.User.email == payload.email,
+            models.User.email == payload.username,
         )
         .first()
     )
@@ -23,4 +23,5 @@ def login(payload: schemas.UserLogin, db: session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
         )
-    return {"message": "Login Success"}
+    access_token=oauth2.create_access_token(data={"user_id":user.id})
+    return {"access_token": access_token,"token_type":"Bearer"}
