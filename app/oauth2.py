0401f13ba75c5d1d46608ae7348124729f18667c
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-06-02 15:41:04 +0530

    fix: JWT Token expiration

--- app/oauth2.py
+++ app/oauth2.py
@@ -7,13 +7,14 @@ from fastapi.security import OAuth2PasswordBearer
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 SECRET_KEY = "9e90aca1f7ea37cb2b7f1a78a7e9ac6b1f616e8f2361fdbb828c7c412b7b33c3"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire_time = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire_time = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode["exp"] = expire_time
+    print(expire_time,to_encode)
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
