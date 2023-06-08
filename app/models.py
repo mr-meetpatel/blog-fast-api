a316cb29df04d220fea05c0367b3466202a4149c
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-06-08 13:30:15 +0530

    feat: Add relationship to the User model from Post model

--- app/models.py
+++ app/models.py
@@ -1,19 +1,23 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
 
 
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False)
-    user_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     is_published = Column(Boolean, server_default="False")
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()")
     )
+    user = relationship("User")
 
 
 class User(Base):
