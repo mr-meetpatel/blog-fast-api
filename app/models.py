a87ca7c2ecd3596e0bb6354ec79d0feabfa20564
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-06-08 11:55:56 +0530

    feat: Add user_id as Foreign Key to Post model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -7,6 +7,7 @@ from sqlalchemy.sql.expression import text
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     is_published = Column(Boolean, server_default="False")
