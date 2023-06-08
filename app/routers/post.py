fb6808e514700b6637d261d8cd696b54501fb07b
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-06-08 11:28:40 +0530

    refactor: Rename user_id variable to current_user

--- app/routers/post.py
+++ app/routers/post.py
@@ -6,7 +6,7 @@ from app.database import get_db, session
 router = APIRouter(prefix="/api/v1",tags=["posts"])
 
 @router.get("/posts", response_model=List[schemas.PostResponse])
-async def get_posts(db: session = Depends(get_db),user_id :int = Depends(oauth2.get_current_user)):
+async def get_posts(db: session = Depends(get_db),current_user :int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts; """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
@@ -14,7 +14,7 @@ async def get_posts(db: session = Depends(get_db),user_id :int = Depends(oauth2.
 
 
 @router.get("/posts/{id}", response_model=schemas.PostResponse)
-async def get_post_by_id(id: int, db: session = Depends(get_db),user_id :int = Depends(oauth2.get_current_user)):
+async def get_post_by_id(id: int, db: session = Depends(get_db),current_user :int = Depends(oauth2.get_current_user)):
     # cursor.execute(
     #     """ SELECT * FROM posts WHERE id=%s; """,
     #     (str(id),),
@@ -34,7 +34,7 @@ async def get_post_by_id(id: int, db: session = Depends(get_db),user_id :int = D
 @router.post(
     "/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse
 )
-async def create_post(payload: schemas.CreatePost, db: session = Depends(get_db),user_id :int = Depends(oauth2.get_current_user)):
+async def create_post(payload: schemas.CreatePost, db: session = Depends(get_db),current_user :int = Depends(oauth2.get_current_user)):
     # cursor.execute(
     #     """INSERT INTO posts("title","content","is_published") VALUES (%s,%s,%s) RETURNING *""",
     #     (payload.title, payload.content, payload.is_published),
@@ -49,7 +49,7 @@ async def create_post(payload: schemas.CreatePost, db: session = Depends(get_db)
 
 
 @router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int, db: session = Depends(get_db),user_id :int = Depends(oauth2.get_current_user)):
+async def delete_post(id: int, db: session = Depends(get_db),current_user :int = Depends(oauth2.get_current_user)):
     # cursor.execute(
     #     """ DELETE FROM posts WHERE id=%s RETURNING *; """,
     #     (str(id)),
@@ -67,7 +67,7 @@ async def delete_post(id: int, db: session = Depends(get_db),user_id :int = Depe
 
 @router.put("/posts/{id}", response_model=schemas.PostResponse)
 async def update_post(
-    id: int, payload: schemas.CreatePost, db: session = Depends(get_db),user_id :int = Depends(oauth2.get_current_user)
+    id: int, payload: schemas.CreatePost, db: session = Depends(get_db),current_user :int = Depends(oauth2.get_current_user)
 ):
     # cursor.execute(
     #     "UPDATE posts SET title=%s,content=%s,is_published=%s WHERE id = %s RETURNING *",
