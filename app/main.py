9c0d6453f49e2085a2ab5df4c254366d22b69142
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-06-08 14:04:34 +0530

    refactor: Comment DB connection code with psycopg2

--- app/main.py
+++ app/main.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI
-from psycopg2.extras import RealDictCursor
 from . import models
 from .database import engine
 from .routers import user,post,authentication
-import psycopg2
-import time
+# from psycopg2.extras import RealDictCursor
+# import psycopg2
+# import time
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-while True:
-    try:
-        db = psycopg2.connect(
-            host="172.18.0.2",
-            database="blog_db",
-            user="root",
-            password="root",
-            cursor_factory=RealDictCursor,
-        )
-        cursor = db.cursor()
-        print("DB Connected")
-        break
-    except Exception as e:
-        print(f"Fail to connect DB {e}")
-        time.sleep(2)
+# For Reference
+# while True:
+#     try:
+#         db = psycopg2.connect(
+#             host="ip address or name",
+#             database="blog_db",
+#             user="user_name",
+#             password="password",
+#             cursor_factory=RealDictCursor,
+#         )
+#         cursor = db.cursor()
+#         print("DB Connected")
+#         break
+#     except Exception as e:
+#         print(f"Fail to connect DB {e}")
+#         time.sleep(2)
 
 
 @app.get("/")
